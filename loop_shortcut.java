e27303a9dbc77a83f10d688ab6a726608eab05c2
chamrouensann <96223936+chamrouensqa@users.noreply.github.com>
2023-12-06 21:32:43 +0700

    Adding loop shortcut and switch case

---
+++ loop_shortcut.java
@@ -0,0 +1,7 @@
+public class loop_shortcut {
+    public static void main(String[] args) {
+        int num = 10;
+        String result = (num >= 10) ? "The number is "+num : "That is another number"+num;
+        System.out.println(result);
+    }
+}
---
+++ switch_case.java
@@ -0,0 +1,19 @@
+public class switch_case {
+    public static void main(String[] args) {
+        int day = 4;
+        switch (day){
+            case 1:
+                System.out.println("day 1");
+                break;
+            case 2:
+                System.out.println("day 2");
+                break;
+            case 3:
+                System.out.println("day 3");
+                break;
+            case 4:
+                System.out.println("day 4");
+                break;
+        }
+    }
+}
